a0aefb66f062bc67a65ebc2b851863b2a84febaf
biskettime <biskettime@gmail.com>
2025-01-15 21:00:43 +0700

    fix: React 버전 수정 및 라우터 설정 추가

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router basename="/your-repo-name">
+      {/* 여기에 라우트 설정을 추가하세요 */}
+    </Router>
+  );
+}
+
+export default App; 
